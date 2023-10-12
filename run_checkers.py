37db47a42991898a96e035149e746f8372de2b3c
Mike Lorang <mike.lorang@govcert.etat.lu>
2023-10-12 09:34:16 +0200

    added theading

--- run_checkers.py
+++ run_checkers.py
@@ -3,21 +3,37 @@ import time
 import sys
 from gamelib import *
 sys.path.append("checkers/saarxiv/")
-import saarxiv
 sys.path.append("checkers/saarlender/")
 import saarchecker
-
-
+import saarxiv
+import saarchecker
+import threading
 
 # Initialize the scheduler
 scheduler = sched.scheduler(time.time, time.sleep)
-# Define the interval in seconds (5 minutes = 300 seconds)
-interval = 5
+
+# Define the interval in seconds (10 seconds)
+interval = 3
+
 global round_g
 round_g = 0
 
+# Define a function to run saarxiv_check in a thread with a timeout
+def run_saarxiv_check(team_nb, round):
+    thread = threading.Thread(target=saarxiv_check, args=(team_nb, round))
+    thread.daemon = True
+    thread.start()
+    thread.join(timeout=15)  # Wait for at most 15 seconds
+
+# Define a function to run saarlender_check in a thread with a timeout
+def run_saarlender_check(team_nb, round):
+    thread = threading.Thread(target=saarlender_check, args=(team_nb, round))
+    thread.daemon = True
+    thread.start()
+    thread.join(timeout=15)  # Wait for at most 15 seconds
+
 def saarxiv_check(team_nb, round):
-    team = saarxiv.Team(2, '', '192.168.42.3'+str(team_nb))    
+    team = saarxiv.Team(2, '', '192.168.42.3' + str(team_nb))
     service = saarxiv.SaarXivInterface(1)
 
     print('[1] Integrity check...')
@@ -32,10 +48,8 @@ def saarxiv_check(team_nb, round):
     flags = service.retrieve_flags(team, round)
     print('Done ({} flags).'.format(flags))
 
-
-def saarlendar_check(team_nb, round):
-    # TEST CODE
-    team = Team(2, 'n00bs', '192.168.42.3'+str(team_nb))
+def saarlender_check(team_nb, round):
+    team = Team(2, 'n00bs', '192.168.42.3' + str(team_nb))
     print("Round:", round)
     service = saarchecker.SaarlendarChecker(2)
 
@@ -50,35 +64,30 @@ def saarlendar_check(team_nb, round):
     print('[3] Retrieve the flags in the next round')
     flags = service.retrieve_flags(team, round)
     print('Done ({} flags).'.format(flags))
-    
 
 # Define a function to execute the task
 def run_checks(teams):
     global round_g
-    round_g +=1
+    round_g += 1
     print("Running service checks started at:", time.ctime())
+    res = []  # Initialize the result list
     for team in teams:
         try:
-            res.append(saarxiv_check(team, round_g))
-            res.append(saarlendar_check(team, round_g))
-        except:
-            return False
-        
-        if False in res:
-            return False
-        else:
-            return True
+            run_saarxiv_check(team, round_g)
+            run_saarlender_check(team, round_g)
+        except Exception as e:
+            print(f"Error occurred: {e}")
 
-
+def schedule_checks():
+    scheduler.enter(interval, 1, schedule_checks, ())
+    run_checks([1])  # Pass teams as needed
 
 def main():
     # Schedule the initial task
-    scheduler.enter(interval, 1, run_checks, ([[1,]]))
-    while True:
-        scheduler.run()
-        #time.sleep(1)
+    scheduler.enter(interval, 1, schedule_checks, ())
+    scheduler.run()
 
-# If run_checkers is called as main program, just run the checkers every 5 minutes.
-#  This automatically places new flags every 5 minutes and checks the services.
+# If run_checkers is called as the main program, just run the checkers every 10 seconds.
+# This automatically places new flags every 10 seconds and checks the services.
 if __name__ == "__main__":
-    main() 
+    main()
